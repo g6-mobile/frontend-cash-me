2845d7b32e9d3f22bf3a25b27066c82cd146cb6c
Erly75 <erlyrivera.750@gmail.com>
2024-06-29 14:01:03 -0500

    logica en el front para actualizar el numero de telefono terminada

--- lib/screen/update/update_phone_number.dart
+++ lib/screen/update/update_phone_number.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pocket_swap_fisi/widget/text.dart'; //subtittleText
 import 'package:pocket_swap_fisi/widget/text_field.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
@@ -15,7 +16,6 @@ class UpdatePhoneNumber extends StatefulWidget {
 }
 
 class _UpdatePhoneNumberState extends State<UpdatePhoneNumber> {
-  
   late TextEditingController _phoneUpdateController;
   
   @override
@@ -24,14 +24,12 @@ class _UpdatePhoneNumberState extends State<UpdatePhoneNumber> {
     _phoneUpdateController = TextEditingController();
   }
   
-  
   @override
   void dispose() {
     _phoneUpdateController.dispose();
     super.dispose();
   }
   
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,12 +77,64 @@ class _UpdatePhoneNumberState extends State<UpdatePhoneNumber> {
 
             BaseElevatedButton(
               text: S.current.ConfirmUpdate,
-              onPressed: () async {}
+              onPressed: () async {
+                if (isValidPeruvianPhoneNumber(_phoneUpdateController.text)){
+                  _showConfirmationDialog();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(S.current.RegisterInvalidPhoneSnackBar)));
+                }
+              }
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _showConfirmationDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm Update Phone Number'),
+          content: Text('Are you sure you want to update your phone number to ${_phoneUpdateController.text}?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              // en este boton va la logica para el back
+              onPressed: () {
+                _performPhoneNumberChange();
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(118, 43, 43, 1)), // Color RGB 762B2B
+              ),
+              child: Text(
+                'Confirm',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _performPhoneNumberChange() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Phone number updated successfully to ${_phoneUpdateController.text}'),
+      ),
+    );
+    _phoneUpdateController.clear();
   }
   
   bool isValidPeruvianPhoneNumber(String phoneNumber) {
